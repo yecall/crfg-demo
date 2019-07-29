d208b1a03fe19aad85268088f49cb66fbc131b8d
dingyouzhen <moses_paul@163.com>
2019-07-29 17:17:28 +0800

    serialize and deserialize pbft message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 //extern crate crossbeam;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
 mod message;
 mod bft_instance;
@@ -7,8 +11,9 @@ mod hashing;
 use std::env;
 use std::thread;
 use std::time::Duration;
+use ed25519_dalek::Keypair;
 
-//use crossbeam::queue::{ArrayQueue, PushError};
+use crossbeam::queue::{ArrayQueue, PushError};
 
 use crate::hashing::blake2_256;
 use crate::message::Message;
@@ -26,12 +31,11 @@ fn main(){
     println!("bft_msg = {:#?}", bft_msg);
     println!("instance = {:#?}", instance);
 
-    //let q = ArrayQueue::new(2);
-    //assert_eq!(q.push('a'), Ok(()));
-
+    //hash practise
     let s = String::from("werqweqwe");
     let h = blake2_256(s.as_bytes());
     println!("hash = {:?}", hex::encode(h));
 
+    let q = ArrayQueue::new(2);
+    assert_eq!(q.push('a'), Ok(()));
 }
-
--- src/message.rs
+++ src/message.rs
@@ -1,62 +1,75 @@
-trait Serialize{
-    fn serialize(&self) -> String;
-    fn deserialize(msg_str: &str) -> Self;
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct PrePrePare{
+    view_number: i64,
+    sequence: i64,
+    replica_id:i64,
+    proposal: String,
+    proposal_hash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct PrePare{
+    view_number: i64,
+    sequence: i64,
+    replica_id:i64,
+    proposal_hash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Commit{
+    view_number: i64,
+    sequence: i64,
+    replica_id:i64,
+    proposal_hash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct ViewChange{
+    view_number: i64,
+    sequence: i64,
+    replica_id:i64,
+    proposal_hash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct ViewChangeFull{
+    view_change: Box<MessageEnv>, //MessageEnv::ViewChange
+    pre_prepare: Box<MessageEnv>, //MessageEnv::PrePrePare
+    prepares: Vec<Box<MessageEnv>>,//MessageEnv::PrePare
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct NewView{
+    view_number: i64,
+    sequence: i64,
+    replica_id:i64,
+    pre_prepare: Box<MessageEnv>, //MessageEnv::PrePrePare
+    view_changes: Vec<Box<MessageEnv>>, //MessageEnv::ViewChange
 }
 
 //对Message 和 MessageEnv 都要实现Serialize trait
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum Message{
     NullMsg,
-    PrePrePare{
-        view_number: i64,
-        sequence: i64,
-        replica_id:i64,
-        proposal: String,
-        proposal_hash: String,
-    },
-    PrePare{
-        view_number: i64,
-        sequence: i64,
-        replica_id:i64,
-        proposal_hash: String,
-    },
-    Commit{
-        view_number: i64,
-        sequence: i64,
-        replica_id:i64,
-        proposal_hash: String,
-    },
-    ViewChange{
-        view_number: i64,
-        sequence: i64,
-        replica_id:i64,
-        proposal_hash: String,
-    },
-    ViewChangeFull{
-        view_change: Box<MessageEnv>, //MessageEnv::ViewChange
-        pre_prepare: Box<MessageEnv>, //MessageEnv::PrePrePare
-        prepares: Vec<Box<MessageEnv>>,//MessageEnv::PrePare
-    },
-    NewView{
-        view_number: i64,
-        sequence: i64,
-        replica_id:i64,
-        pre_prepare: Box<MessageEnv>, //MessageEnv::PrePrePare
-        view_changes: Vec<Box<MessageEnv>>, //MessageEnv::ViewChange
-    },
+    PrePrePare(PrePrePare),
+    PrePare(PrePare),
+    Commit(Commit),
+    ViewChange(ViewChange),
+    ViewChangeFull(ViewChangeFull),
+    NewView(NewView),
 }
 
-impl Serialize for Message{
-    fn serialize(&self) -> String {
-        String::from("Message")
+impl Message{
+    fn serialize(msg: &Message) -> String {
+        serde_json::to_string(msg).unwrap()
     }
 
     fn deserialize(msg_str: &str) -> Message{
-        Message::NullMsg
+        serde_json::from_str(msg_str).unwrap()
     }
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct MessageEnv{
     round_number: i64,
     message: Message,
@@ -75,3 +88,59 @@ impl MessageEnv{
     fn signature(&self) -> &str{ &self.signature }
 }
 
+impl MessageEnv{
+    fn serialize(msg_env: &MessageEnv) -> String {
+        serde_json::to_string(msg_env).unwrap()
+    }
+
+    fn deserialize(msg_env_str: &str) -> MessageEnv{
+        serde_json::from_str(msg_env_str).unwrap()
+    }
+}
+
+#[test]
+fn test_serialize_message(){
+    let prepare = Message::PrePare(PrePare{view_number: 10, sequence: 100, replica_id:3, proposal_hash: String::from("sdf")});
+
+    let prepare_serialized = Message::serialize(&prepare);
+    println!("prepare serialized: {}", prepare_serialized);
+
+    let prepare_deserialized = Message::deserialize(&prepare_serialized);
+    println!("prepare deserialize: {:#?}", prepare_deserialized);
+
+    assert_eq!(prepare, prepare_deserialized);
+}
+
+#[test]
+fn test_serialize_message_env(){
+    let new_view = Message::NewView(NewView{
+        view_number: 10,
+        sequence: 100,
+        replica_id:3,
+        pre_prepare: Box::new(MessageEnv{
+            round_number: 5,
+            message: Message::NullMsg,
+            signature: String::from("signature for pre_prepare")
+        }),
+        view_changes: vec![
+            Box::new(MessageEnv{
+                round_number: 5,
+                message: Message::Commit(Commit{
+                    view_number: 10, sequence: 100, replica_id:3, proposal_hash: String::from("commit hash")
+                }),
+                signature: String::from("signature for new_view1")
+            }),
+            Box::new(MessageEnv{
+                round_number: 6, message: Message::NullMsg, signature: String::from("signature for new_view2")
+            })
+        ],
+    });
+
+    let new_view_serialized= Message::serialize(&new_view);
+    println!("new_view serialized: {}", new_view_serialized);
+
+    let new_view_deserialized = Message::deserialize(&new_view_serialized);
+    println!("new_view deserialize: {:#?}", new_view_deserialized);
+
+    assert_eq!(new_view, new_view_deserialized);
+}
